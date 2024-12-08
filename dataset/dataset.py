29ea29223de7ec631ac0a3f1e694c7158edd3a83
bluff-king <vubkk67@gmail.com>
2024-12-08 16:16:59 +0700

    reorganize glove lstm CNN(vgg16)+LSTM.

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -62,3 +62,50 @@ def transformer_collate(batch):
 
     next_tokens = torch.tensor([item[2] for item in batch])
     return images, captions, next_tokens
+
+
+class LstmDataset(Dataset):
+    def __init__(self, root_dir, captions, image_ids, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+
+        self.captions_augmented = []
+        self.imgs_augmented = []
+        self.next_token = []
+        for caption, img in zip(captions, image_ids):
+            numericalized_caption = [stoi("<SOS>")]
+            numericalized_caption += numericalize(caption)
+            numericalized_caption.append(stoi("<EOS>"))
+            for idx in range(min(len(numericalized_caption), CAPTIONS_LENGTH) - 1):
+                self.imgs_augmented.append(img)
+                # pre-pad here
+                padded_caption = numericalized_caption[:idx+1] + [stoi('<PAD>')] * CAPTIONS_LENGTH
+                padded_caption = padded_caption[:CAPTIONS_LENGTH]
+                self.captions_augmented.append(padded_caption)
+                self.next_token.append(numericalized_caption[idx + 1])
+            
+    def __len__(self):
+        return len(self.captions_augmented)
+
+    def __getitem__(self, idx):
+        caption = torch.tensor(self.captions_augmented[idx])
+
+        img_id = self.imgs_augmented[idx]
+        img_path = os.path.join(self.root_dir, img_id)
+        image = Image.open(img_path).convert("RGB")
+        if self.transform:
+            image = self.transform(image)
+        # next_token = itov_single(self.next_token[idx])
+
+        # return image, caption, next_token
+        return image, caption, self.next_token[idx]
+
+def lstm_collate(batch):
+    images = [item[0].unsqueeze(0) for item in batch]
+    images = torch.cat(images, dim=0)
+
+    captions = [item[1].unsqueeze(0) for item in batch]
+    captions = torch.cat(captions, dim=0)
+
+    next_tokens = torch.tensor([item[2] for item in batch])
+    return images, captions, next_tokens
