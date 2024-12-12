ee4f6842044b69b2a2a08a40f697d1c722bb388a
thanh309 <vuthnh.td@gmail.com>
2024-12-13 06:17:07 +0700

    gui finished

--- GUI.py
+++ GUI.py
@@ -1,11 +1,15 @@
 import gradio as gr 
 import torch 
 from models.transformer import ImageCaptioningTransformer
+from models.lstm import ImageCaptioningLstm as LstmGlove
+from models.lstm_attention import ImageCaptioningLstm as LstmAtt
+from models.own_embedder_lstm import ImageCaptioningLstm as LstmCBOW
 from embedding.embedding import stoi, vocab_npa
 from PIL import Image 
 import re
 from nltk.tokenize.treebank import TreebankWordDetokenizer
 from dataset.dataset import transform
+from embedding.own_embedding import own_vocab_npa, own_stoi
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -32,7 +36,10 @@ checkpoint_paths = {
     "Transformer20": f"{CHECKPOINT_PATH}transformer_caplen20.pth",
     "Transformer25": f"{CHECKPOINT_PATH}transformer_caplen25.pth",
     "Transformer20_COCO": f"{CHECKPOINT_PATH}transformer_caplen20_coco.pth",
-    "Transformer25_COCO": f"{CHECKPOINT_PATH}transformer_caplen25_coco.pth", 
+    "Transformer25_COCO": f"{CHECKPOINT_PATH}transformer_caplen25_coco.pth",
+    'LSTM25_Glove': f'{CHECKPOINT_PATH}glove_lstm25.pth',
+    'LSTM25_CBOW': f'{CHECKPOINT_PATH}own_embedder_lstm25.pth',
+    'LSTM20_Attention': f'{CHECKPOINT_PATH}lstm_attention20.pth'
 }
 
 d = TreebankWordDetokenizer()
@@ -47,6 +54,20 @@ def load_model(model_name, checkpoint_path):
     
     if model_name in ['Transformer25', 'Transformer25_COCO']:
         model = ImageCaptioningTransformer(cap_len=25).to(device)
+    
+    if model_name == 'LSTM25_Glove':
+        model = LstmGlove().to(device)
+    
+    if model_name == 'LSTM25_CBOW':
+        model = LstmCBOW().to(device)
+    
+    if model_name == 'LSTM20_Attention':
+        model = LstmAtt().to(device)
+        checkpoint = torch.load(checkpoint_path, weights_only=True)
+        model.load_state_dict(checkpoint)
+        model.eval()
+        
+        return model 
 
     assert model != None, 'what'
 
@@ -72,16 +93,60 @@ def generate_caption(image, model_name, temperature, num_captions, max_unk_wait)
     img = transform(Image.fromarray(image).convert("RGB"))
     img = torch.unsqueeze(img, 0).to(device)
 
-    if model_name in ['Transformer20', 'Transformer20_COCO']:
+    if model_name in ['Transformer20', 'Transformer20_COCO', 'LSTM20_Attention']:
         max_caption_len = 20
     
-    elif model_name in ['Transformer25', 'Transformer25_COCO']:
+    elif model_name in ['Transformer25', 'Transformer25_COCO', 'LSTM25_Glove', 'LSTM25_CBOW']:
         max_caption_len = 25
     
-    else: 
-        max_caption_len = 15
-        
+    # else: 
+    #     max_caption_len = 15
+
     ans = []
+
+    if model_name == 'LSTM25_CBOW':
+        own_pad_idx = own_stoi('<PAD>')
+        own_sos_idx = own_stoi('<SOS>')
+        own_eos_idx = own_stoi('<EOS>')
+        own_unk_idx = own_stoi('<UNK>')
+        for _ in range(num_captions):
+            caption = [[own_pad_idx for _ in range(max_caption_len)]]
+            caption = torch.tensor(caption).to(device)
+            caption[0, 0] = own_sos_idx
+            caption_str = ['<SOS>']
+
+            unk_wait = 0
+            next_idx = 1
+
+            with torch.no_grad():
+                while next_idx < max_caption_len:
+                    output_logits = all_models[model_name](img, caption)
+                    output_logits = output_logits / temperature
+
+                    output_probs = torch.softmax(output_logits, dim=-1)
+                    output = torch.multinomial(output_probs, num_samples=1).item()
+
+                    if output == own_unk_idx:
+                        unk_wait += 1
+                        if unk_wait < max_unk_wait:
+                            continue
+                    
+                    unk_wait = 0
+
+                    # caption_str += f'{vocab_npa[output]} '
+                    caption_str.append(own_vocab_npa[output])
+                    caption[0, next_idx] = output
+
+                    if output == own_eos_idx:
+                        break
+
+                    next_idx += 1
+
+            caption_detokenized = d.detokenize(caption_str)
+            ans.append(re.sub(r'[<SOS><EOS>]', '', caption_detokenized))
+            
+        return "\n".join(ans)
+
     for _ in range(num_captions):
         caption = [[pad_idx for _ in range(max_caption_len)]]
         caption = torch.tensor(caption).to(device)
